7e131b5bfe31180f69a373db1cef4694c6be1fb6
sahalan hasan <mythsahalan@yahoo.com>
2020-10-07 21:13:07 +0600

    stripe webhook appolied

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -23,7 +23,9 @@ exports.getCheckoutSessions = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} tour`,
         description: tour.summary,
-        images: [`/img/tours/${tour.imageCover}`],
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+        ],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1
@@ -47,7 +49,7 @@ exports.getCheckoutSessions = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -63,7 +65,7 @@ exports.webhookCheckout = (req, res, next) => {
   } catch (error) {
     return res.status(400).send(`Webhok error: ${error.message}`);
   }
-  if (event.type === 'checkout.session.complete') {
+  if (event.type === 'checkout.session.completed') {
     createBookingCheckout(event.data.object);
     res.status(200).json({ received: true });
   }
